74700e35ad2cb1c59919c6831658613a42ab34cb
shakizWasTaken <yahya2003rhemmali@gmail.com>
2022-04-29 10:01:38 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form,Button } from 'react-bootstrap';
 
 function App() {
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(Object.fromEntries(data.entries()));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Form id='form-container' onSubmit={handleSubmit}>
+        
+        <h1>Login</h1>
+        
+        <Form.Group id='form-group' >
+        <Form.Label>first name:</Form.Label>
+        <Form.Control name="fName" placeholder="Enter email"/>
+        </Form.Group>
+        
+        <Form.Group id='form-group' >
+        <Form.Label>last name:</Form.Label>
+        <Form.Control name="lName" placeholder="Enter email"/>
+        </Form.Group>
+
+        <Form.Group id='form-group' >
+        <Form.Label>email:</Form.Label>
+        <Form.Control name="email" placeholder="Enter email"/>
+        </Form.Group>
+
+        <Button type='submit'>Login</Button>
+
+      </Form>
     </div>
   );
 }
